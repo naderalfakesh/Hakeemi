ea3b5b4b8148e58110a0b220b5eee16a348fc5b6
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-04-03 17:43:56 +0300

    Add theme colors and spacing

---
+++ src/theme/colors.ts
@@ -0,0 +1,34 @@
+export const palette = {
+  primary: {
+    base: 'rgba(0, 188, 212, 1)',
+    light: 'rgba(214, 250, 255, 1)',
+    dark: 'rgba(144, 180, 206, 1)',
+  },
+  secondary: {
+    base: 'rgba(237, 243, 247, 1)',
+  },
+  danger: {
+    base: 'rgba(255, 87, 34, 1)',
+  },
+  grey: {
+    base: 'rgba(240, 240, 240, 1)',
+    light: 'rgba(251, 251, 251, 1)',
+    dark: 'rgba(144, 144, 144, 1)',
+  },
+  white: {
+    base: 'rgba(255, 255, 255, 1)',
+  },
+  black: {
+    base: 'rgba(0, 0, 0, 1)',
+  },
+};
+
+export default {
+  primary: palette.primary.base,
+  secondary: palette.secondary.base,
+  danger: palette.danger.base,
+  light: palette.grey.light,
+  dark: palette.grey.dark,
+  white: palette.white.base,
+  black: palette.black.base,
+};
