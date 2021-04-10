86bb732472e94f15e1eb27847e5de99d2024ae29
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-04-10 19:53:56 +0300

    Update Jest rules and configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['jest'],
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  env: {
+    'jest/globals': true,
+  },
 };
