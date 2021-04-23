36c9752a524179fd09e0299678c04a15321b78ca
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-04-23 21:28:27 +0300

    Add typscript declaration to i18n

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,19 +2,26 @@ import i18next from 'i18next';
 import en from './en';
 import ar from './ar';
 
+export const resources = {
+  en,
+  ar,
+} as const;
+
 const i18n = i18next.createInstance();
 i18n.init({
   fallbackLng: 'en',
   lng: 'en',
   supportedLngs: ['en', 'ar'],
   defaultNS: 'common',
-  resources: {
-    en,
-    ar,
-  },
+  resources,
   react: {
     useSuspense: false,
   },
 });
+
+declare module 'react-i18next' {
+  type DefaultResources = typeof resources['en'];
+  interface Resources extends DefaultResources {}
+}
 
 export default i18n;
