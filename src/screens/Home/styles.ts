97c5c0733b4d813523b310dcf9895aec93e3cb75
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-04-23 03:24:46 +0300

    Add Home screen

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,9 +1,17 @@
 import { StyleSheet } from 'react-native';
 import colors from '../../theme/colors';
+import spacing from '../../theme/spacing';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
+    paddingHorizontal: spacing.medium2,
   },
+  doctorIcon: { marginBottom: -1 }, // A fix for extra space in svg
+  title: { marginVertical: spacing.small1 },
+  search: { marginBottom: spacing.mini3 },
+  card: { marginBottom: spacing.medium2 },
+  recent: { marginBottom: spacing.medium2 },
+  innerContainer: { marginTop: spacing.small3 },
 });
