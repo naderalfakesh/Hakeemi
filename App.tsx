5d3843096d5b2a2797ae070da4f6bf85f20f41ca
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-04-23 05:46:44 +0300

    Add i18n localization support

--- App.tsx
+++ App.tsx
@@ -10,12 +10,16 @@ import React, { ReactNode } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootStack from './src/navigation/RootStack';
 import { StatusBar } from 'react-native';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './src/i18n';
 
 const App: () => ReactNode = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" translucent={true} />
-      <RootStack />
+      <I18nextProvider i18n={i18n}>
+        <RootStack />
+      </I18nextProvider>
     </NavigationContainer>
   );
 };
