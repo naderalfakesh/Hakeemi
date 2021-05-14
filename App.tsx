dc42ebbf443b7c985b5bf40ca312dde080a0100f
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-05-14 10:26:14 +0300

    Fix safe area insets when header is hidden

--- App.tsx
+++ App.tsx
@@ -12,15 +12,22 @@ import RootStack from './src/navigation/RootStack';
 import { StatusBar } from 'react-native';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './src/i18n';
+import {
+  initialWindowMetrics,
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
 
 const App: () => ReactNode = () => {
+  console.log(initialWindowMetrics);
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" translucent={true} />
-      <I18nextProvider i18n={i18n}>
-        <RootStack />
-      </I18nextProvider>
-    </NavigationContainer>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" translucent={true} />
+        <I18nextProvider i18n={i18n}>
+          <RootStack />
+        </I18nextProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
