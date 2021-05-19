89c30d2309dd351cb33a2fed308eaf3d9ddbbb2e
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-05-20 01:30:56 +0300

    Fix reanimated install for Android

--- android/app/src/main/java/com/doctorapp/MainApplication.java
+++ android/app/src/main/java/com/doctorapp/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.react.bridge.JSIModulePackage; // Required by reanimated library
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // Required by reanimated library
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -33,6 +35,11 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // Required by reanimated library
+      }
       };
 
   @Override
