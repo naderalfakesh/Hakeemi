f8117bb3afc1794da53c74e47d7d17b381967b64
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-05-22 22:07:52 +0300

    Update project name to Hakeemi app

--- android/app/src/main/java/com/doctorapp/MainApplication.java
+++ android/app/src/main/java/com/doctorapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.doctorapp;
+package com.Hakeemi;
 
 import android.app.Application;
 import android.content.Context;
@@ -69,7 +69,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.doctorapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.Hakeemi.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
